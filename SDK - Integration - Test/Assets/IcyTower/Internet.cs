504ffa947bd7cb452bcb23b44de5e20d147a10ec
HasanGanover <hasan@ganover.com>
2020-04-12 02:33:57 +0300

    [ New ] Commit Internet.cs that handles the connection issues results.

---
+++ SDK - Integration - Test/Assets/IcyTower/Internet.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using UnityEngine;
+
+public class Internet 
+{
+    // Check Internet Connection
+    public static bool ConnectionTest()
+    {
+        try
+        {
+            using (var client = new WebClient())
+
+            using (var stream = client.OpenRead("http://www.google.com"))
+            {
+                Debug.Log("Connection Succeed.");
+
+                return true;
+            }
+        }
+        catch
+        {
+            Debug.LogError("Connection Failed.");
+
+            return false;
+        }
+    }
+
+}
