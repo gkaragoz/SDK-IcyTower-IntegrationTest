5717ee23d4b98ab1877caa0c503b9bf8dfe2070a
HasanGanover <hasan@ganover.com>
2020-04-12 02:28:14 +0300

    [ New ] Commit GameManager

---
+++ SDK - Integration - Test/Assets/IcyTower/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(ProgressBar))]
+public class GameManager : MonoBehaviour
+{
+    #region SINGLETON
+
+    public static GameManager current;
+
+    private void Awake()
+    {
+        if (current == null) { current = this; }
+
+        else if (current != this) { Destroy(gameObject); } 
+    }
+
+    #endregion
+
+    /********************************************************/
+    // LOAD SYSTEM
+
+    private LoadManager _loadManager;
+
+    /********************************************************/
+
+    private void Start()
+    {
+        _loadManager = new LoadManager(GetComponent<ProgressBar>());
+    }
+
+}
